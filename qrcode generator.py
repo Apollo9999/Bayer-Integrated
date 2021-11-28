05f49866a4fc0c04f72c0b252ceb3ddeee5909a1
Apollo9999 <skjkiran0212@gmail.com>
2021-11-28 22:38:22 +0530

    Add files via upload
    ok

---
+++ qrcode generator.py
@@ -0,0 +1,20 @@
+# !pip install qrcode[pil]
+
+import qrcode
+from PIL import Image
+
+# img = qrcode.make('Your input text')
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data('input url')
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+
+img.save("sample.png")
+
